3d8fc3052d0aa1900b8c659fb666a3e3e9dbfc74
nas <y.softvalley@hey.com>
2024-05-20 00:32:25 +0900

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,19 @@
 import type { Metadata } from "next";
+import type { Viewport } from "next";
+
 import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
 
 export const metadata: Metadata = {
   title: "Easy MFA",
   description: "Easy MFA",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 const RootLayout = ({
